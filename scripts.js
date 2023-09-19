e02e4ea6a53d651369ce57849e4cda952a171c73
deigo-lps <diego.lopes0909@gmail.com>
2023-09-19 16:00:33 -0300

    commit inicial

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const customHeader = (scrollPoint) =>{
+  const mobileButton = document.querySelector(".header__mobile-button");
+  const mobileMenu = document.querySelector(".header ul");
+  
+  mobileButton.addEventListener("click", () => {
+    mobileButton.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+  });
+  
+  const scrollPoint = document.querySelector(scrollPoint);
+  const headerBlack = document.querySelector(".header__black");
+  
+  window.addEventListener("scroll", () => {
+    if (!isElementInViewport(scrollPoint)) {
+      headerBlack.classList.add("active");
+    } else headerBlack.classList.remove("active");
+  });
+}
